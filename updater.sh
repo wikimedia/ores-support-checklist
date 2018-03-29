625eec2f88f9feb8e34a65558616ef091c800852
Amir Sarabadani <ladsgroup@gmail.com>
2018-03-30 00:23:22 +0200

    Add updater.sh

---
+++ updater.sh
@@ -0,0 +1,5 @@
+cd /data/project/ores-support-checklist/www/python/src
+git pull
+webservice restart
+source /data/project/ores-support-checklist/www/python/venv/bin/activate
+python data_updater.py
